80f03c41d1413b12df1d355d8a0f0fceb50d3aa8
intaglioplate312 <helenelizabeth@me.com>
2017-09-25 09:45:31 -0500

    Terminal test working

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,16 @@
     ./webpack.config.js
 */
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
 module.exports = {
-    entry: './App/index.js',
+    entry: './client/index.js',
     output: {
         path: path.resolve('dist'),
         filename: 'index_bundle.js'
@@ -14,5 +22,6 @@ module.exports = {
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
     },
+    // add this line
     plugins: [HtmlWebpackPluginConfig]
 }
